3c434fa73662872cbb59a9639d263d94eed198cb
Beenyameen <beenyameen@hotmail.com>
2026-03-17 16:48:21 +0000

    chore: bump version to 1.0.3 for publishing

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="rvl",
-    version="1.0.2",
+    version="1.0.3",
     description="Python bindings for the RVL lossless compression algorithm.",
     long_description=long_description,
     long_description_content_type="text/markdown",
